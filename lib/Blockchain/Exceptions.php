86a08a026c5041fddff4c6638b008ce3ec6c2957
Clark <clarkmoody@gmail.com>
2014-10-27 13:55:15 -0500

    Add CredentialsError exception.

--- lib/Blockchain/Exceptions.php
+++ lib/Blockchain/Exceptions.php
@@ -4,4 +4,5 @@ class Blockchain_Error extends Exception {}
 class Blockchain_HttpError extends Blockchain_Error {}
 class Blockchain_ApiError extends Blockchain_Error {}
 class Blockchain_FormatError extends Blockchain_Error {}
+class Blockchain_CredentialsError extends Blockchain_Error {}
 ?>
