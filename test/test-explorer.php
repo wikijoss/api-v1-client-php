27187ecaff6a3955b47ee7054db23f593a2297b5
Clark <clarkmoody@gmail.com>
2014-10-28 12:49:47 -0500

    Expand tests

--- test/test-explorer.php
+++ test/test-explorer.php
@@ -9,7 +9,20 @@ if(file_exists('code.txt')) {
 
 $Blockchain = new Blockchain($api_code);
 
-$blocks23 = $Blockchain->Explorer->getBlocksAtHeight(23);
-print_r($blocks23);
+// List all blocks at a certain height
+print_r($Blockchain->Explorer->getBlocksAtHeight(23));
 
-?>
+// Get block by index
+$block = $Blockchain->Explorer->getBlockAtIndex(100000);
+print_r($block);
+
+// Get previous block by hash
+$hash = $block['prev_block'];
+print_r($Blockchain->Explorer->getBlock($hash));
+
+// First mining reward transaction
+print_r($Blockchain->Explorer->getTransaction('4a5e1e4baab89f3a32518a88c31bc87f618f76673e2cc77ab2127b7afdeda33b'));
+
+
+// Output log of activity
+print_r($Blockchain->log);
