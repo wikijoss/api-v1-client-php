1656464fa506a0734f17aaeed98538d125667ab8
Clark <clarkmoody@gmail.com>
2014-11-04 13:30:01 -0600

    Block Explorer and test complete.

--- lib/Blockchain/Explorer.php
+++ lib/Blockchain/Explorer.php
@@ -66,6 +66,114 @@ class Explorer {
 		}
 		return $outputs;
 	}
+
+	public function getLatestBlock() {
+		return new LatestBlock($this->blockchain->get('latestblock', array('format'=>'json')));
+	}
+
+	public function getUnconfirmedTransactions() {
+		$json = $this->blockchain->get('unconfirmed-transactions', array('format'=>'json'));
+		$txn = array();
+		if(array_key_exists('txs', $json)) {
+			foreach ($json['txs'] as $tx) {
+				$txn[] = new Transaction($tx);
+			}
+		}
+		return $txn;
+	}
+
+	/* Get blocks for a specific day, provided UNIX timestamp, in seconds.
+	*/
+	public function getBlocksForDay($unix_time=0) {
+		$time_ms = strval($unix_time) . '000';
+		return $this->processSimpleBlockJSON($this->blockchain->get('blocks/'.$time_ms, array('format'=>'json')));
+	}
+
+	/* Get blocks for a specific mining pool.
+	*/
+	public function getBlocksByPool($pool) {
+		return $this->processSimpleBlockJSON($this->blockchain->get('blocks/'.$pool, array('format'=>'json')));
+	}
+
+	private function processSimpleBlockJSON($json) {
+		$blocks = array();
+		if(array_key_exists('blocks', $json)) {
+			foreach ($json['blocks'] as $block) {
+				$blocks[] = new SimpleBlock($block);
+			}
+		}
+		return $blocks;
+	}
+
+	public function getInventoryData($hash) {
+		return new InventoryData($this->blockchain->get('inv/'.$hash, array('format'=>'json')));
+	}
+}
+
+class InventoryData {
+    public $hash;
+    public $type;
+    public $initial_time;
+    public $initial_ip;
+    public $nconnected;
+    public $relayed_count;
+    public $relayed_percent;
+
+    public function __construct($json) {
+    	if(array_key_exists('hash', $json))
+    		$this->hash = $json['hash'];
+    	if(array_key_exists('type', $json))
+    		$this->type = $json['type'];
+    	if(array_key_exists('initial_time', $json))
+    		$this->initial_time = $json['initial_time'];
+    	if(array_key_exists('initial_ip', $json))
+    		$this->initial_ip = $json['initial_ip'];
+    	if(array_key_exists('nconnected', $json))
+    		$this->nconnected = $json['nconnected'];
+    	if(array_key_exists('relayed_count', $json))
+    		$this->relayed_count = $json['relayed_count'];
+    	if(array_key_exists('relayed_percen', $json))
+    		$this->relayed_percen = $json['relayed_percen'];
+    }
+}
+
+class SimpleBlock {
+    public $height;
+    public $hash;
+    public $time;
+    public $main_chain;
+
+    public function __construct($json) {
+    	if(array_key_exists('height', $json))
+    		$this->height = $json['height'];
+    	if(array_key_exists('hash', $json))
+    		$this->hash = $json['hash'];
+    	if(array_key_exists('time', $json))
+    		$this->time = $json['time'];
+    	if(array_key_exists('main_chain', $json))
+    		$this->main_chain = $json['main_chain'];
+    }
+}
+        
+class LatestBlock {
+    public $hash;
+    public $time;
+    public $block_index;
+    public $height;
+    public $tx_indexes = array();
+
+    public function __construct($json) {
+    	if(array_key_exists('hash', $json))
+    		$this->hash = $json['hash'];
+    	if(array_key_exists('time', $json))
+    		$this->time = $json['time'];
+    	if(array_key_exists('block_index', $json))
+    		$this->block_index = $json['block_index'];
+    	if(array_key_exists('height', $json))
+    		$this->height = $json['height'];
+    	if(array_key_exists('txIndexes', $json))
+			$this->tx_indexes[] = $json['txIndexes'];
+    }
 }
 
 class Address {
--- test/test-explorer.php
+++ test/test-explorer.php
@@ -13,21 +13,40 @@ $Blockchain = new Blockchain($api_code);
 print_r($Blockchain->Explorer->getBlocksAtHeight(1));
 
 // Get block by index
-$block = $Blockchain->Explorer->getBlockAtIndex(100000);
-print_r($block);
+// $block = $Blockchain->Explorer->getBlockAtIndex(100000);
+// print_r($block);
 
 // Get previous block by hash
-$hash = $block->previous_block;
-print_r($Blockchain->Explorer->getBlock($hash));
+// $hash = $block->previous_block;
+// print_r($Blockchain->Explorer->getBlock($hash));
 
 // First mining reward transaction
-print_r($Blockchain->Explorer->getTransaction('0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098'));
+// print_r($Blockchain->Explorer->getTransaction('0e3e2357e806b6cdb1f70b54c3a3a17b6714ee1f0e68bebb44a74b1efd512098'));
 
 // Get details of a single address
-print_r($Blockchain->Explorer->getAddress('1AqC4PhwYf7QAyGBhThcyQCKHJyyyLyAwc'));
+// print_r($Blockchain->Explorer->getAddress('1AqC4PhwYf7QAyGBhThcyQCKHJyyyLyAwc'));
 
 // Get unspent outputs for addresses
-print_r($Blockchain->Explorer->getUnspentOutputs(array('1AqC4PhwYf7QAyGBhThcyQCKHJyyyLyAwc', '1PfcDu4n11Dv7rNexM1AxrNWqkEgwCvYWD')));
+// print_r($Blockchain->Explorer->getUnspentOutputs(array('1AqC4PhwYf7QAyGBhThcyQCKHJyyyLyAwc', '1PfcDu4n11Dv7rNexM1AxrNWqkEgwCvYWD')));
+
+// Get the latest block
+// print_r($Blockchain->Explorer->getLatestBlock());
+
+// Get blocks from the past
+// print_r($Blockchain->Explorer->getBlocksForDay(1262325600));
+
+// Get blocks from a mining pool
+// print_r($Blockchain->Explorer->getBlocksByPool('Eligius'));
+
+// Get unconfirmed transactions
+// $tx = $Blockchain->Explorer->getUnconfirmedTransactions();
+// print_r($tx);
+
+// Get inventory data for an unconfirmed transaction in $tx
+// if(count($tx) > 0)
+//	print_r($Blockchain->Explorer->getInventoryData($tx[0]->hash));
+
+
 
 // Output log of activity
 print_r($Blockchain->log);
